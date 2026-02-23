fe9d1a7f813db36ca992ebdfad561e0a046fd940
Raksha Karn <karnraksha41@gmail.com>
2026-02-24 02:44:16 +0545

    Added filter based search

--- students.py
+++ students.py
@@ -1,18 +1,41 @@
 import uuid
-
+import json
+from typing import Optional, List
 
 class StudentManager:
-    def __init__(self, name, email, contact):
-        self.name = name
-        self.email = email
-        self.contact = contact
-        self.enrolled_courses = []
-        self.students = [{}]
+    def __init__(self):
+        self.students = []
 
-    def add_student(self):
-        self.students.append({
-            "Id": uuid.uuid4(),
-            "Name": self.name,
-            "Email": self.email,
-            "Contact": self.contact
-        })
+    def add_student(self, name:str, email:str, contact:str, student_class:str, faculty:str, degree:str, section:str):
+        student = {
+            "id": str(uuid.uuid4()),
+            "name": name,
+            "email": email,
+            "contact": contact,
+            "student_class": student_class,
+            "faculty": faculty,
+            "degree": degree,
+            "section": section
+        }
+        self.students.append(student)
+
+    def view_students(self, faculty: Optional[str] = None, student_class: Optional[str] = None, degree: Optional[str] = None, section: Optional[str] = None) -> List[dict]:
+        result = self.students
+        if faculty:
+            return [s for s in result if s["faculty"] == faculty]
+        
+        if student_class:
+            return [s for s in result if s["student_class"] == student_class]
+        
+        if degree:
+            return [s for s in result if s["degree"] == degree]
+        
+        if section:
+            return [s for s in result if s["section"] == section]
+        
+        return result
+
+
+
+    
+
