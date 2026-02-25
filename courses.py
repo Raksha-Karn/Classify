b0fef3c278a07b3f31987ff8333acc2c2b4760b5
Raksha Karn <karnraksha41@gmail.com>
2026-02-25 22:49:32 +0545

    Added grouped and paginated view

--- courses.py
+++ courses.py
@@ -1,4 +1,6 @@
 import uuid 
+from collections import defaultdict
+from typing import List
 
 
 class CourseManager:
@@ -18,7 +20,7 @@ class CourseManager:
 
         self.courses.append(course)
 
-    def edit_course(self, course_id, **updates):
+    def edit_course(self, course_id:str, **updates):
         for course in self.courses:
             if course["id"] == course_id:
                 for key, value in updates.items():
@@ -27,4 +29,42 @@ class CourseManager:
                 return course
         return None
     
-   
+    def view_courses(self):
+        grouped = defaultdict(
+            lambda: defaultdict(
+                lambda: defaultdict(list)
+            )
+        )
+
+        for course in self.courses:
+            faculty = course["faculty"]
+            degree = course["degree"]
+            level = f"{course["level_type"].capitalize()} {course["level_value"]}"
+            grouped = [faculty][degree][level].append(course)
+        return grouped
+
+    def view_course_by_id(self, course_id):
+        for course in self.courses:
+            if course["id"] == course_id:
+                return course
+        return None
+    
+    def paginated_view(self, data: List[dict], page: int = 1, limit: int = 10):
+        start = (page - 1)*limit
+        end = start + limit
+        return data[start:end]
+
+    def delete_course(self):
+        pass
+
+    def assign_teacher(self):
+        pass
+
+    def enroll_student(self):
+        pass
+
+    def get_students_in_course(self):
+        pass
+
+    def get_courses_of_student(self):
+        pass
