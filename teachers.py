001fc511c576115e1416f21105e396a884e487ab
Raksha Karn <karnraksha41@gmail.com>
2026-02-26 23:54:21 +0545

    Added view teachers

--- teachers.py
+++ teachers.py
@@ -1,11 +1,13 @@
 import uuid
+from collections import defaultdict
+from typing import Optional, List
 
 
 class TeacherManager:
     def __init__(self):
         self.teachers = []
 
-    def add_teacher(self, name, classes, contact, meta):
+    def add_teacher(self, name: str, classes: list, contact: str, meta: str):
         teacher = {
             "id": str(uuid.uuid4()),
             "name": name,
@@ -16,7 +18,7 @@ class TeacherManager:
 
         self.teachers.append(teacher)
 
-    def edit_teacher(self, teacher_id, **updates):
+    def edit_teacher(self, teacher_id: str, **updates):
         for teacher in self.teachers:
             if teacher["id"] == teacher_id:
                 for key, value in updates.items():
@@ -24,4 +26,21 @@ class TeacherManager:
                         teacher[key] = value
                 return teacher
         return None
+    
+    def view_teachers(self, faculty: Optional[str] = None, student_class: Optional[str] = None, degree: Optional[str] = None, section: Optional[str] = None) -> List[dict]):
+        result = self.teachers
+        if faculty:
+            return [s for s in result if s["faculty"] == faculty]
+        
+        if student_class:
+            return [s for s in result if s["student_class"] == student_class]
+        
+        if degree:
+            return [s for s in result if s["degree"] == degree]
+        
+        if section:
+            return [s for s in result if s["section"] == section]
+        
+        return result
+
                 
