6b16ce650f9d63eb9231e7d07af3bce90d393a89
Raksha Karn <karnraksha41@gmail.com>
2026-02-26 23:49:47 +0545

    Added edit teachers functionality

--- teachers.py
+++ teachers.py
@@ -15,3 +15,13 @@ class TeacherManager:
         }
 
         self.teachers.append(teacher)
+
+    def edit_teacher(self, teacher_id, **updates):
+        for teacher in self.teachers:
+            if teacher["id"] == teacher_id:
+                for key, value in updates.items():
+                    if key in teacher and value is not None:
+                        teacher[key] = value
+                return teacher
+        return None
+                
